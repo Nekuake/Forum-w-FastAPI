6178892fb5724128d65b3d5a8105437eb0f0d358
Nekuake - Alejandro Hurtado <ruizale55@gmail.com>
2025-06-18 01:22:42 +0200

    main file completed

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.database import Base, engine
+from src.forum.router import forum_router, post_router
 from configuration import settings
-app = FastAPI()
+app = FastAPI(
+    title="Forum implemented with FastAPI",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(forum_router)
+app.include_router(post_router)
 
 os.makedirs(settings.USER_CONTENT_DIR, exist_ok=True)
 
